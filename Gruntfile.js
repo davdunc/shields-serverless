a0b9fc54718f4ea5dcbd3e0e523191055d5c6ec2
Mandus Momberg <git@momberg.me>
2017-08-07 18:39:16 -0700

    Made changes to Grunt Build and Added BuildSpec for CodeBuild.

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-zip');
 
-  // Default task(s).
+   // Default task(s).
   grunt.registerTask('default', ['eslint','copy','zip']);
 
 };
